d801422fac1586dbb38333deaa3716bb793260be
Mahalinga Raya <mahalingaraya.mr@gmail.com>
2026-06-06 12:25:42 +0530

    Create BookingController.java

---
+++ src/main/java/com/mahatechmahi/cricsync/controller/BookingController.java
@@ -0,0 +1,35 @@
+package com.mahatechmahi.cricsync.controller;
+
+import com.mahatechmahi.cricsync.entity.Booking;
+import com.mahatechmahi.cricsync.service.BookingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/bookings")
+public class BookingController {
+
+    @Autowired
+    private BookingService bookingService;
+
+    @PostMapping
+    public Booking createBooking(@RequestBody Booking booking) {
+        return bookingService.saveBooking(booking);
+    }
+
+    @GetMapping
+    public List<Booking> getAllBookings() {
+        return bookingService.getAllBookings();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Booking> getBookingById(@PathVariable Long id) {
+        Optional<Booking> booking = bookingService.getBookingById(id);
+        return booking.map(ResponseEntity::ok)
+                      .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+}
