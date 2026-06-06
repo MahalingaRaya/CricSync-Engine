2d444d92fd59e2beff1794590fe26649b6438e0d
Mahalinga Raya <mahalingaraya.mr@gmail.com>
2026-06-07 05:29:26 +0530

    Create MatchPlayerRepository.java

---
+++ src/main/java/com/mahatechmahi/cricsync/repository/MatchPlayerRepository.java
@@ -0,0 +1,13 @@
+package com.mahatechmahi.cricsync.repository;
+
+import com.mahatechmahi.cricsync.entity.MatchPlayer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MatchPlayerRepository extends JpaRepository<MatchPlayer, Long> {
+    // This allows us to instantly find all 11 players for a specific team in a specific match
+    List<MatchPlayer> findByMatchIdAndTeamName(Long matchId, String teamName);
+}
