10a139511cfed27cac1c64316214dc5630583512
Mahalinga Raya <mahalingaraya.mr@gmail.com>
2026-06-06 14:04:38 +0530

    Update JobController.java

--- src/main/java/com/mahatechmahi/cricsync/controller/JobController.java
+++ src/main/java/com/mahatechmahi/cricsync/controller/JobController.java
@@ -1,35 +1,41 @@
 package com.mahatechmahi.cricsync.controller;
 
-import com.mahatechmahi.cricsync.entity.Job;
+import com.mahatechmahi.cricsync.entity.MarketplaceJob;
 import com.mahatechmahi.cricsync.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/jobs")
+@CrossOrigin(origins = "*")
 public class JobController {
 
     @Autowired
     private JobService jobService;
 
     @PostMapping
-    public Job createJob(@RequestBody Job job) {
-        return jobService.saveJob(job);
+    public ResponseEntity<MarketplaceJob> createJob(@RequestBody MarketplaceJob job) {
+        return ResponseEntity.ok(jobService.saveJob(job));
     }
 
     @GetMapping
-    public List<Job> getAllJobs() {
-        return jobService.getAllJobs();
+    public ResponseEntity<List<MarketplaceJob>> getAllJobs() {
+        return ResponseEntity.ok(jobService.getAllJobs());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Job> getJobById(@PathVariable Long id) {
-        Optional<Job> job = jobService.getJobById(id);
-        return job.map(ResponseEntity::ok)
-                  .orElseGet(() -> ResponseEntity.notFound().build());
+    public ResponseEntity<MarketplaceJob> getJobById(@PathVariable Long id) {
+        return jobService.getJobById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteJob(@PathVariable Long id) {
+        jobService.deleteJob(id);
+        return ResponseEntity.noContent().build();
     }
 }
